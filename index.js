bf05ce6900489c6a9be8c4bbf1b7cbec5713d876
esdraswite <witeesdras@gmail.com>
2023-07-19 22:54:20 +0200

    create databse credentials

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import mysql from 'mysql2'
 
 const app = express();
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"malkiadashbord"
+})
 
 const PORT = 8800;
 
 app.listen(PORT, ()=>{
-    console.log("The app is running to the port", PORT)
+    console.log("The app is running to the port", PORT);
 })
