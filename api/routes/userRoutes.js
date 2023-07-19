fca50002c7025c8dbca004fe327a3a4383e1fa27
esdraswite <witeesdras@gmail.com>
2023-07-20 01:27:36 +0200

    add route fonctions

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,36 +3,49 @@ import db from "../configs/database.js";
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/signup', (req, res) => {
 
     const { username, password } = req.body;
 
     const q = "INSERT INTO user(`username`, `password`) VALUES (?)";
 
-    db.query(q, [username, password], (error, result) => {
+    db.query(q, [username, password], (error, data) => {
         if (error) {
-           return res.status(500).json({
+            return res.status(500).json({
                 message: 'There is an internal error',
                 error: error
             })
-        }else{
+        } else {
             res.status(200).json({
-                message:'sucess',
-                data:result
+                message: "user created",
+                data: data
             })
         }
     })
-
-
-    res.status(200).json({
-        message: "user created"
-    })
 });
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        message: "User retreived",
+router.post('/signin', (req, res) => {
 
+    const { username, password } = req.body;
+
+    // const q = `SELECT * FROM user WHERE username = ${username}`;
+    const q = `SELECT * FROM user WHERE username = '${username}'`;
+    // const q = `SELECT * FROM user`;
+
+    db.query(q, (error, data) => {
+
+        if (error) return res.status(500).json(error);
+
+        if (data.length > 0) {
+            if (!(data[0].password === password)) return res.json('Mot de passe incorrect');
+            res.status(200).json({
+                message: 'sucess'
+            })
+        } else {
+            return res.status(400).json({
+                message: 'Utilisateur non existant'
+            })
+        }
     })
 })
 
