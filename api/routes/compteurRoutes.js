ce990b41faf878788440d392fe9599fb55283f5d
esdraswite <witeesdras@gmail.com>
2023-07-20 01:51:07 +0200

    complete routes fonctions

--- api/routes/compteurRoutes.js
+++ api/routes/compteurRoutes.js
@@ -1,23 +1,43 @@
-import express, { json } from "express";
+import express from "express";
 import db from "../configs/database.js";
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.status(200).json({
-        message: "compteur created"
+
+    const q = "INSERT INTO `compteur`(`airtemperature`, `airhumidity`, `groundhumidity`, `citernlevel`, `fleur_idfleur`) VALUES(?)";
+
+    const values = [
+        req.body.airtemperature,
+        req.body.airhumidity,
+        req.body.groundhumidity,
+        req.body.airtemperature,
+        req.body.citernlevel,
+        req.body.fleur_idfleur,
+    ];
+
+    db.query(q, values, (error, data) => {
+        
+        if (error) return res.status(500).json(error);
+
+        res.status(200).json({
+            message: 'success',
+            data: data
+        })
+
     })
+
 });
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
 
     const q = `SELECT * FROM compteur`;
 
-    db.query(q, (error, data)=>{
-        if(error) return res.status(500).json(error);
+    db.query(q, (error, data) => {
+        if (error) return res.status(500).json(error);
         res.status(200).json({
-            message:'success',
-            data:data
+            message: 'success',
+            data: data
         })
     })
 })
--- api/routes/fleurRoutes.js
+++ api/routes/fleurRoutes.js
@@ -1,16 +1,40 @@
 import express from 'express';
+import db from '../configs/database';
 const router = express.Router();
 
-router.post('/', (req, res)=>{
-    res.status(200).json({
-        message:'Fleur poster'
+router.post('/', (req, res) => {
+
+    const values = [req.body.type]
+
+    const q = "INSERT INTO `fleur`(`type`, `user_iduser`) VALUES (?)";
+
+    db.query(q, values, (error, data) => {
+
+        if (error) return res.status(500).json(error);
+
+        res.status(200).json({
+            message: 'success',
+            data: data
+        })
     })
+
+
 });
 
-router.get('/', (req, res)=>{
-    res.status(200).json({
-        message:'Fleur retreived'
+router.get('/', (req, res) => {
+
+    const q = "SELECT `idfleur`, `type`, `user_iduser` FROM `fleur`";
+
+    db.query(q, (error, data) => {
+        
+        if (error) return res.status(500).json(error);
+
+        res.status(200).json({
+            message: 'success',
+            data: data
+        })
     })
+
 })
 
 export default router;
