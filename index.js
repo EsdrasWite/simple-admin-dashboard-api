175f22c4c07836999910fc9d699ff597af4a7be6
esdraswite <witeesdras@gmail.com>
2023-07-20 00:31:39 +0200

    modify index

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 import express from 'express';
-import mysql from 'mysql2';
 import userRoutes from './api/routes/userRoutes.js';
 import fleurRoutes from './api/routes/fleurRoutes.js';
 import compteurRoutes from './api/routes/compteurRoutes.js';
 
 const app = express();
-
-const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"malkiadashbord"
-});
 
 app.use('/user', userRoutes);
 app.use('/fleur', fleurRoutes);
@@ -20,8 +12,6 @@ app.use('/compteur', compteurRoutes);
 app.get("/", (req, res)=>{
     res.json('Welcome to malkia_server dashbord')
 });
-
-
 
 const PORT = 8800;
 
