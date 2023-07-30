f06b9cfd5f8677e4d5f5d0e6a1bec2e2567bca45
esdraswite <witeesdras@gmail.com>
2023-07-30 23:20:31 +0200

    add middleware

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import compteurRoutes from './api/routes/compteurRoutes.js';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use('/user', userRoutes);
 app.use('/fleur', fleurRoutes);
